f11546ad3b912bb8a4bb427683fa0424fa77ec68
mhmdali23 <mhmdali23101@gmail.com>
2024-11-26 06:12:13 +0200

    Initial setup with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Include all your Angular templates and components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
